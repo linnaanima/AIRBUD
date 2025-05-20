b8313a2f47b673c4fce6f9c311f8755ff2fa9b25
linnaanima <linnanima@googlemail.com>
2025-05-20 21:05:25 +0200

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -39,12 +39,13 @@ def app():
     }
     .stTabs [data-baseweb="tab"] {
         height: 50px;
+        wide: 20px;
         white-space: pre-wrap;
         background-color: #f0f2f6;
-        border-radius: 4px 4px 3px 3px;
-        gap: 3px;
+        border-radius: 4px 4px 0px 0px;
+        gap: 1px;
         padding-top: 10px;
-        padding-bottom: 20px;
+        padding-bottom: 10px;
     }
     .stTabs [aria-selected="true"] {
         background-color: #e6f0ff;
