3c04e715e071b52892b10b4fa230bd5d0fe942aa
linnaanima <linnanima@googlemail.com>
2025-05-20 21:10:56 +0200

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -34,7 +34,7 @@ def app():
         height: 50px;
         white-space: pre-wrap;
         background-color: #f0f2f6;
-        border-radius: 4px 4px 4px 4px;
+        border-radius: 4px 4px 0px 0px;
         gap: 1px;
         padding-top: 10px;
         padding-bottom: 10px;
